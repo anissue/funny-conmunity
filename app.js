bf70befcf0fce4340977eb0962a6de989340d5ac
NanShanJi <786726541@qq.com>
2017-07-21 23:35:33 +0800

    session 添加进去了

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+
+
+var config  = require('./config');
+
 var express = require('express');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+var bodyParser = require('body-parser');
 var path    = require('path');
 var ejs     = require('ejs');
+
+
 var app     = express();
 var webRouter = require('./web_router');
+var apiRouter = require('./api_router');
 
 var staticDir = path.join(__dirname, 'public');
 var viewsDir  = path.join(__dirname, 'views');
@@ -13,7 +23,19 @@ app.set('views', viewsDir);
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
+app.use(session({
+	secret: config.session_secret,
+	store: new mongoStore({url: config.db}),
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 app.use('/public', express.static(staticDir));
+app.use('/api', apiRouter);
 app.use('/', webRouter);
 
 // app.listen(80, '192.168.25.18');

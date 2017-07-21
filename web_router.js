99817e472b6c412f80376ad7e4bd12a9a8a1a78f
NanShanJi <786726541@qq.com>
2017-07-21 23:42:26 +0800

    把这些假数据替换掉

--- web_router.js
+++ web_router.js
@@ -1,56 +1,56 @@
 var express = require('express');
-
+var weibo   = require('./middlewares/weibo_auth');
+var qq      = require('./controllers/qq');
+var user    = require('./controllers/user');
 var router  = express.Router();
+
+
 
 router.get('/', function (req, res, next) {
 	res.render('index', {
-		config: {
-			state_login: true
-		}
+		user: req.user
 	});
 });
 
-router.get('/user/new', function (req, res, next) {
-	res.render('./user/new', {
-		config: {
-			state_login: true
-		}
-	});
-});
+router.get('/user/new', user.new);
 
 router.get('/post/up', function (req, res, next) {
 	res.render('./post/upload', {
-		config: {
-			state_login: true
-		}
+		user: req.user
 	});
 });
 
 router.get('/user/edit', function (req, res, next) {
 	res.render('./user/edit', {
-		config: {
-			state_login: true
-		}
+		user: req.user
 	});
 });
 
 
 router.get('/user/edit', function (req, res, next) {
 	res.render('./user/edit', {
-		config: {
-			state_login: true
-		}
+		user: user
 	});
 });
 
 
-// 需要过滤掉系统关键词
-router.get('/user/:name', function (req, res, next) {
-	res.render('./user/index', {
-		config: {
-			state_login: true
-		}
+// // 需要过滤掉系统关键词
+// router.get('/user/:name', function (req, res, next) {
+//
+// 	res.render('./user/index', {
+// 		user: user
+// 	});
+// });
+
+router.get('/auth/wb', function (req, res, next) {
+	weibo.getAccessToken(req.query.code, function (err, access) {
+		weibo.getInfo(access, function (err, info) {
+			res.send(info);
+		});
 	});
 });
+
+// qq登录进入
+router.get('/auth/qq', qq.sign);
 
 module.exports = router;

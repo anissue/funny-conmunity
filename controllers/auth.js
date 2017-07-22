78b5e4deff79ece32d50017f91ab91f6d2bad913
NanShanJi <786726541@qq.com>
2017-07-23 00:25:57 +0800

    第三方登录的总入口文件夹

---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+var qq = require('../middlewares/qq_auth');
+var User = require('../models').User;
+var wb = require('../middlewares/wb_auth');
+
+// qq 登录
+exports.qqSign = auth.bind(qq);
+
+// 微博 登录
+exports.wbSign = auth.bind(wb);
+
+function auth (req, res, next) {
+	var This = this;
+	var user = {state_login: false};
+	var code = req.query.code;
+	var idField = This.authType + 'id';
+
+	This.getAccessToken(code, function (err, access) {
+		if (err) next(err);
+		var condition = {};
+		condition = This.authType === 'qq' ? condition = {qqid: access.id} : condition = {weiboid: access.id};
+
+		User.find(condition, function (err, result) {
+
+			// 新用户
+			if (result.length === 0) {
+				// 获得授权得来信息
+				This.getInfo(access, function (err, info) {
+					if (err) next(err);
+
+					// 把信息临时保存起来
+					req.session.temInfo = info;
+					req.session.access = access;
+					return res.redirect('/user/new');
+				});
+			}
+
+			// 老用户
+			if (result.length > 0) {
+				// 刷新 token
+				var token = User.createToken();
+				User.update(condition, {$set: {token: token}}, function (err, result){
+					if (err) return next(err);
+					req.session.token = token;
+					return res.redirect('/')
+				});
+			}
+		});
+	});
+}

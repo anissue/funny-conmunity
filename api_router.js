e30bd89316715ea4f3ebcc1aeb9c83a52988047f
NanShanJi <786726541@qq.com>
2017-07-23 00:24:43 +0800

    新增站内登录，上传头像，修改资料的api路由

--- api_router.js
+++ api_router.js
@@ -8,4 +8,12 @@ var router  = express.Router();
 // 增加新用户
 router.post('/user/new', user.new);
 
+// 站内登录
+router.post('/user/login', user.login);
+
+// 上传头像
+router.post('/user/avatar', user.uploadAvatar);
+
+// 修改资料
+router.post('/user/edit', user.edit);
 module.exports = router;

f287b4f88e4c21d4154c3f87668a8d2464ca8a66
NanShanJi <786726541@qq.com>
2017-07-23 23:28:08 +0800

    修改了授权的方式，给每个页面路由请求增加登录状态

--- app.js
+++ app.js
@@ -5,26 +5,29 @@ var config  = require('./config');
 var express = require('express');
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path    = require('path');
 var ejs     = require('ejs');
 
 
 var app     = express();
+var userMiddlewares = require('./middlewares/auth');
 var webRouter = require('./web_router');
 var apiRouter = require('./api_router');
-var auth      = require('./middlewares/auth');
 
 var staticDir = path.join(__dirname, 'public');
 var avatarDir = path.join(__dirname, 'avatar');
 var viewsDir  = path.join(__dirname, 'views');
 
 
-
-
 app.set('views', viewsDir);
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
+
+
+app.use('/avatar', express.static(avatarDir));
+app.use('/public', express.static(staticDir));
 
 app.use(session({
 	secret: config.session_secret,
@@ -32,13 +35,12 @@ app.use(session({
 	resave: false,
 	saveUninitialized: false
 }));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/avatar', express.static(avatarDir));
-app.use('/public', express.static(staticDir));
+app.use(cookieParser(config.session_secret));
 app.use('/api', apiRouter);
-app.use('/', auth.authUser);
+app.use('/', userMiddlewares.authUser);
+
+
 app.use('/', webRouter);
 
 // app.listen(80, '192.168.25.18');

af1dfba9ed155b42e0b2da02e0da4a4318046d8d
NanShanJi <786726541@qq.com>
2017-07-23 23:27:23 +0800

    新增上传帖子图片

--- api_router.js
+++ api_router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var user    = require('./api/user');
-
+var post    = require('./api/post');
 
 var router  = express.Router();
 
@@ -16,4 +16,10 @@ router.post('/user/avatar', user.uploadAvatar);
 
 // 修改资料
 router.post('/user/edit', user.edit);
+
+// 返回登录状态
+router.post('/user/islogin', user.isLogin);
+
+// 上传帖子图片
+router.post('/post/upload', post.uploadImg);
 module.exports = router;

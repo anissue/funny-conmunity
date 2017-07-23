7f3cb0ad7fa600b8c72b658cafa86868b636a174
NanShanJi <786726541@qq.com>
2017-07-23 23:37:38 +0800

    增加帖子页面路由

--- web_router.js
+++ web_router.js
@@ -2,32 +2,31 @@ var express = require('express');
 var weibo   = require('./middlewares/wb_auth');
 var qq      = require('./controllers/qq');
 var user    = require('./controllers/user');
+var post    = require('./controllers/post');
 var auth    = require('./controllers/auth');
 var router  = express.Router();
-
+var config  = require('./config');
 
 
 router.get('/', function (req, res, next) {
 	res.render('index', {
-		user: req.user
+		user: req.user,
+		config: config
 	});
 });
 
 router.get('/user/new', user.new);
 router.get('/user/login', user.login);
 
-router.get('/post/up', function (req, res, next) {
-	res.render('./post/upload', {
-		user: req.user
-	});
-});
+router.get('/post/up', post.upload);
 
 router.get('/user/edit', user.edit);
 
 
 router.get('/user/edit', function (req, res, next) {
+
 	res.render('./user/edit', {
-		user: user
+		user: req.user
 	});
 });
 
@@ -35,16 +34,10 @@ router.use('/uploadredirect', function (req, res, next) {
 	res.send('ahhhhh');
 });
 
+
 router.get('/user/out', user.out);
 
-// // 需要过滤掉系统关键词
-// router.get('/user/:name', function (req, res, next) {
-//
-// 	res.render('./user/index', {
-// 		user: user
-// 	});
-// });
-
+// 微博登录进入
 router.get('/auth/wb', auth.wbSign);
 
 // qq登录进入

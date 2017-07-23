203105550fb5b3967d26c15f21ea355b19371d03
NanShanJi <786726541@qq.com>
2017-07-23 23:27:06 +0800

    把上传头像的逻辑分离出来

--- api/user.js
+++ api/user.js
@@ -1,7 +1,6 @@
-var fs         = require('fs');
 var path       = require('path');
-var formidable = require('formidable');
-
+var tools      = require('./tools');
+var uploadImg  = require('./uploadImg');
 var User       = require('../models').User;
 
 // 创建新用户
@@ -12,8 +11,13 @@ exports.new = function (req, res, next) {
 	var legal = User.legal(data);
 
 	if (legal.states !== 1) return res.json(legal);
-
-	data.type = req.session.temInfo === undefined || req.session.temInfo === null ? 'dnd' : req.session.temInfo.type ;
+	console.log(req.session.temInfo);
+	if (req.session.temInfo === undefined || req.session.temInfo === null) {
+		data.type = 'dnd';
+	} else {
+		data.type = req.session.temInfo.type ;
+		data.avatar = req.session.temInfo.avatar;
+	}
 
 	data.token = User.createToken();
 	data.password = User.cryptoPassword(data.password);
@@ -52,55 +56,29 @@ exports.login = function (req, res, next) {
 	});
 };
 
-// 上传头像
+// 上传图片
 // 上传头像需要使用到 ifream 技术来获得返回值 所以只能重定向
 // 以前使用其他语言做过很多爬虫，从中得到结论永远不要相信传过来的数据
 exports.uploadAvatar = function (req, res, next) {
 	var token = req.session.token;
-	// 放头像的文件夹
-	var avatarDir =  path.join(__dirname, '..', 'avatar');
 	User.find({token: token}, function (err, result) {
-		if (err) return parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
+		if (err) return tools.parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
 
-		if (result.length < 1) parseRedirect({ states: -6, hint  : 'token已过期', data  : '' }, res);
+		if (result.length < 1) tools.parseRedirect({ states: -6, hint  : 'token已过期', data  : '' }, res);
 
-		var form = formidable.IncomingForm();
+		return uploadImg.saveLocal(req, res, next, {
+			maxSize: 1024 * 1024,
+			fileName: result[0].loginname,
+			dir: path.join(__dirname, '..', 'avatar')
+		}, function (userFileName) {
+			// 更新头像地址
+			User.update({token: token}, {$set: {avatar: userFileName}}, function (err, result) {
+				if (err) return tools.parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
 
-		form.uploadDir = avatarDir;
-		form.keepExtensions = true;
-
-		form.parse(req, function (err, fields, file) {
-			if (err)
-				return parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
-
-			if (file.avatar === undefined)
-				return parseRedirect({ states: -2, hint  : '上传的东东错误', data  : '' }, res);
-
-			file = file.avatar;
-			if (file.size > 1024 * 1024)
-				return parseRedirect({ states: -3, hint  : '头像超过1m', data  : '' }, res);
-
-			var type = ['image/png','image/jpeg','image/gif'];
-			if (type.indexOf(file.type) === -1)
-				return parseRedirect({ states: -4, hint  : '图片类型不正确', data  : '' }, res);
-
-			// 新文件名
-			var userFileName =   path.join(result[0].loginname + path.extname(file.name));
-			fs.rename(file.path, path.join(avatarDir, userFileName), function (err) {
-				if (err) return parseRedirect({ states: -5, hint  : '图片地址有误', data  : '' }, res);
-
-				// 更新头像地址
-				User.update({token: token}, {$set: {avatar: userFileName}}, function (err, result) {
-					if (err) return parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
-
-					return parseRedirect({ states: 1, hint  : '头像上传完成', data  :  userFileName}, res);
-				});
+				return tools.parseRedirect({ states: 1, hint  : '头像上传完成', data  :  userFileName}, res);
 			});
-
 		});
 	});
-
-
 };
 
 // 修改资料
@@ -111,34 +89,86 @@ exports.edit = function (req, res, next) {
 		wb: req.body.wb
 	};
 	if (data.description.length > 150 || data.qq.length > 100 || data.wb.length > 100) {
-		return res.send({ states: -2, hint  : '数据有误' });
+		return res.json({ states: -2, hint  : '数据有误' });
 	}
 	var token = req.session.token;
 	User.find({token: token}, function (err, result) {
-		if (err) return res.send({ states: -1, hint  : '服务器错误' });
+		if (err) return res.json({ states: -1, hint  : '服务器错误' });
 
-		if (result.length < 1) return res.send({ states: -2, hint  : 'token过期' });
+		if (result.length < 1) return res.json({ states: -2, hint  : 'token过期' });
 
 
 		User.update({token: token}, {$set: data}, function (err, result) {
-			if (err) return res.send({ states: -1, hint  : '服务器错误' });
+			if (err) return res.json({ states: -1, hint  : '服务器错误' });
 
-			return res.send({ states: 1, hint  : '更新成功' });
+			return res.json({ states: 1, hint  : '更新成功' });
 		});
 
 	});
 };
 
-/*
-* 把指定的 json 装换为重定向地址
-* json = {
-* 	states: *,
-* 	hint: *,
-*   data: *
-* }
-* */
+// 返回登录状态
+exports.isLogin = function (req, res, next) {
+	var token = req.session.token;
+	User.find({token: token}, function (err, result) {
+		if (err) return res.json({ states: -1, hint  : '服务器错误' });
+
+		if (result.length < 1) return res.json({ states: -2, hint  : 'token过期' });
+		return res.json({ states: 1, hint  : '登录' });
+	});
+};
 
 
-function parseRedirect (json, res) {
-	res.redirect('/uploadredirect/' + json.states + '/' + json.hint + '/' + json.data);
-}
+
+/*exports.uploadAvatar = function (req, res, next) {
+ var token = req.session.token;
+ // 放头像的文件夹
+ var avatarDir =  path.join(__dirname, '..', 'avatar');
+ User.find({token: token}, function (err, result) {
+ if (err) return parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
+
+ if (result.length < 1) parseRedirect({ states: -6, hint  : 'token已过期', data  : '' }, res);
+
+ var form = formidable.IncomingForm();
+
+ form.uploadDir = avatarDir;
+ form.keepExtensions = true;
+
+ form.parse(req, function (err, fields, file) {
+ if (err)
+ return parseRedirect({states: -1, hint: '服务器错误', data: ''}, res);
+
+ if (file.avatar === undefined)
+ return parseRedirect({ states: -2, hint  : '上传的东东错误', data  : '' }, res);
+
+ file = file.avatar;
+
+ if (file.size > 1024 * 1024) {
+ fs.unlink(file.path, function (err) {
+ return parseRedirect({ states: -3, hint  : '头像超过1m', data  : '' }, res);
+ });
+ }
+
+ var type = ['image/png','image/jpeg','image/gif'];
+ if (type.indexOf(file.type) === -1) {
+ fs.unlink(file.path, function (err) {
+ return parseRedirect({ states: -4, hint  : '图片类型不正确', data  : '' }, res);
+ });
+ }
+
+ // 新文件名
+ var userFileName =   path.join(result[0].loginname + path.extname(file.name));
+ fs.rename(file.path, path.join(avatarDir, userFileName), function (err) {
+ if (err) return parseRedirect({ states: -5, hint  : '图片地址有误', data  : '' }, res);
+
+ // 更新头像地址
+ User.update({token: token}, {$set: {avatar: userFileName}}, function (err, result) {
+ if (err) return parseRedirect({ states: -1, hint  : '服务器错误', data  : '' }, res);
+
+ return parseRedirect({ states: 1, hint  : '头像上传完成', data  :  userFileName}, res);
+ });
+ });
+
+ });
+ });
+ };*/

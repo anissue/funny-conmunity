f0177f00d1dafd76727a4401c289804f585dcc61
NanShanJi <786726541@qq.com>
2017-08-05 10:42:45 +0800

    新增不需要登陆也可以进入的功能

--- api/middlewares.js
+++ api/middlewares.js
@@ -10,3 +10,14 @@ exports.auth = function (req, res, next) {
 		next();
 	});
 };
+
+// 不需要登陆也能进入
+exports.tryAuth = function (req, res, next) {
+	var token = req.session.token;
+	User.find({token: token}, function (err, user) {
+		if (err) return res.json({states: -1, hint: '服务器错误'});
+		if (user.length < 1) return next();
+		req.user = user[0];
+		next();
+	});
+};
